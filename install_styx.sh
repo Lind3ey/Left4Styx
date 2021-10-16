328322a2bdc1b5cd3d845184b0a1d30ab993571e
lindsey <lindsey>
2021-10-16 23:16:42 +0800

    fix Makefile

--- install_styx.sh
+++ install_styx.sh
@@ -2,7 +2,13 @@
 
 L4D2="$HOME/steam/server/left4dead2"
 PLUGINDIR="$L4D2/addons/sourcemod/plugins"
+if [ ! -d "$L4D2" ]; then
+    fail "Missing $L4D2 directory"
+fi
 
+if [ ! -d "$PLUGINDIR" ]; then
+    mkdir -p $PLUGINDIR
+fi
 echo "L4D2: $L4D2"
 
 cd scripting
@@ -13,9 +19,10 @@ mkdir -p $PLUGINDIR/optional/styx
 mv styx_compiled/* $PLUGINDIR/optional/styx/
 cd ..
 
-echo "moving addons to $L4D2"
+echo "copying addons to $L4D2"
 cp ./addons $L4D2/ -r
-echo "moving cfg to $L4D2"
+echo "copying cfg to $L4D2"
 cp ./cfg    $L4D2/ -r
-echo "moving scripts to $L4D2"
+echo "copying scripts to $L4D2"
 cp ./scripts  $L4D2/ -r
+echo "install complete"
